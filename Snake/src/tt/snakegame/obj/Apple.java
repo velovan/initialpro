fdf06c6be4a39d461848a9c0071bb50d545d652f
velovan <velovan@users.noreply.github.com>
2022-07-18 16:54:11 +0300

    Update Apple.java

--- Snake/src/tt/snakegame/obj/Apple.java
+++ Snake/src/tt/snakegame/obj/Apple.java
@@ -9,13 +9,13 @@ public class Apple{
 	SnakeGame main;
 	
 	public Apple(int stX, int stY){
-	   posX = stX;
-	   posY = stY;
+	  posX = stX;
+	  posY = stY;
 	}
 	
 	public void setRandomPosition(){
-	   posX = (int) (Math.random() *main.WIDTH );
-	   posY = (int) (Math.random() * main.HEIGHT);
+	  posX = (int) (Math.random() *main.WIDTH );
+	  posY = (int) (Math.random() * main.HEIGHT);
 	}
 	
 }
