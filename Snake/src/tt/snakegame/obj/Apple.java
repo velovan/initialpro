884f8fceeaed50633e40b76902fd145c5957f6e9
velovan <velovan@users.noreply.github.com>
2025-10-14 10:24:47 +0300

    Update Apple.java

--- Snake/src/tt/snakegame/obj/Apple.java
+++ Snake/src/tt/snakegame/obj/Apple.java
@@ -2,7 +2,7 @@ package tt.snakegame.obj;
 
 import tt.snakegame.SnakeGame;
 
-public class Apple {
+public class Apple{
  
 	public int posX;
 	public int posY;
@@ -18,3 +18,4 @@ public class Apple {
 	  posY = (int) (Math.random() * main.HEIGHT);
 	}
 }
+
